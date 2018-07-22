f7371cb914fdf2baf64c9e0ec2ca86fd3844afb7
Axel Wettervik <axel.wettervik@gmail.com>
2018-07-22 21:45:45 +0900

    Improve readability of map_file test.

--- src/map_file_test.c
+++ src/map_file_test.c
@@ -2,6 +2,21 @@
 
 #include "platform.h"
 
+void
+map_view_of_file(mapped_file* file, int64_t offset, int64_t size)
+{
+	printf("set offset to: %jd\n", offset);
+	file->offset = offset;
+	
+	printf("create view of %jd bytes\n", size);
+	mapped_file_view view = filesystem_file_view_map(file, size);
+
+	printf("view base: %p\n", view.base);
+	printf("view mapped: %p\n", view.mapped);
+
+	filesystem_file_view_unmap(&view);
+}
+
 int 
 main(int argc, char* argv[])
 {
@@ -16,24 +31,10 @@ main(int argc, char* argv[])
     mapped_file file = filesystem_create_mapped_file(argv[1], 1, 0);
     printf("mapped file of size: %jd\n", file.file_size);
 
-    printf("create view of %jd bytes\n", file.file_size / 8);
-    mapped_file_view view = filesystem_file_view_map(&file, file.file_size / 8);
+    map_view_of_file(&file, 0,                     file.file_size / 4);
+    map_view_of_file(&file, file.file_size / 2,    file.file_size / 4);
+    map_view_of_file(&file, file.file_size - 1024, file.file_size / 4);
 
-    printf("view base: %p\n", view.base);
-    printf("view mapped: %p\n", view.mapped);
-
-    filesystem_file_view_unmap(&view);
-
-    printf("set offset to: %jd\n", file.file_size / 2);
-    file.offset = file.file_size / 2;
-
-    printf("create view of %jd bytes\n", file.file_size / 8);
-    view = filesystem_file_view_map(&file, file.file_size / 8);
-
-    printf("view base: %p\n", view.base);
-    printf("view mapped: %p\n", view.mapped);
-
-    filesystem_file_view_unmap(&view);
     filesystem_destroy_mapped_file(&file);
 
     return 0;
